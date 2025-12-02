f73ae9d590e3fd6bac739b91a046f85e4095b83b
Sean Oijukye <s.oijukye@gmail.com>
2025-12-02 17:42:48 +0000

    feat: Replace navbar implementation with TopNavBar widget for consistency

--- lib/views/about_us_page.dart
+++ lib/views/about_us_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/footer_widget.dart';
+import 'package:union_shop/widgets/top_navbar.dart';
 import '../widgets/dropdown_menu.dart';
 
 class AboutUsPage extends StatelessWidget {
@@ -23,96 +24,10 @@ class AboutUsPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  // Header (identical to HomeScreen)
-                  // Top banner
-                  Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    color: const Color(0xFF4d2963),
-                    child: const Text(
-                      'BIG SALE! OUR ESSENTIAL RANGE HAS DROPPED IN PRICE! OVER 20% OFF! COME GRAB YOURS WHILE STOCK LASTS!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  ),
-                  // Navbar
-                  Container(
-                    height: 35,
-                    color: Colors.white,
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            navigateToHome(context);
-                          },
-                          child: Image.network(
-                            'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
-                            height: 18,
-                            fit: BoxFit.cover,
-                            errorBuilder: (context, error, stackTrace) {
-                              return Container(
-                                color: Colors.grey[300],
-                                width: 18,
-                                height: 18,
-                                child: const Center(
-                                  child: Icon(Icons.image_not_supported,
-                                    color: Colors.grey),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                        const Spacer(),
-                        ConstrainedBox(
-                          constraints: const BoxConstraints(maxWidth: 600),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.search,
-                                  size: 18,
-                                  color: Colors.grey,
-                                ),
-                                padding: const EdgeInsets.all(8),
-                                constraints: const BoxConstraints(
-                                  minWidth: 32,
-                                  minHeight: 32,
-                                ),
-                                onPressed: placeholderCallbackForButtons,
-                              ),
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.shopping_bag_outlined,
-                                  size: 18,
-                                  color: Colors.grey,
-                                ),
-                                padding: const EdgeInsets.all(8),
-                                constraints: const BoxConstraints(
-                                  minWidth: 32,
-                                  minHeight: 32,
-                                ),
-                                onPressed: placeholderCallbackForButtons,
-                              ),
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.menu,
-                                  size: 18,
-                                  color: Colors.grey,
-                                ),
-                                padding: const EdgeInsets.all(8),
-                                constraints: const BoxConstraints(
-                                  minWidth: 32,
-                                  minHeight: 32,
-                                ),
-                                onPressed: () => SimpleDropdownMenu.show(context, 107),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                  TopNavBar(
+                    onLogoTap: (ctx) => navigateToHome(ctx),
+                    onSearch: placeholderCallbackForButtons,
+                    onBag: placeholderCallbackForButtons,
                   ),
 
                   // About Us content
