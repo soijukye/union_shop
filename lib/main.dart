7a2504916e24da1948d1baf581e412122fda55b5
Sean Oijukye <s.oijukye@gmail.com>
2025-12-02 18:52:53 +0000

    feat: Refactor UnionShopApp to use StatefulWidget and integrate CartModel for state management

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,22 @@ import 'package:union_shop/views/about_us_page.dart';
 import 'package:union_shop/views/product_page.dart';
 import 'package:union_shop/views/home_screen.dart';
 import 'package:union_shop/views/cart_page.dart';
+import 'package:union_shop/models/cart_model.dart';
 
 
 void main() {
   runApp(const UnionShopApp());
 }
 
-class UnionShopApp extends StatelessWidget {
+class UnionShopApp extends StatefulWidget {
   const UnionShopApp({super.key});
+
+  @override
+  State<UnionShopApp> createState() => _UnionShopAppState();
+}
+
+class _UnionShopAppState extends State<UnionShopApp> {
+  final CartModel cartModel = CartModel();
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +28,12 @@ class UnionShopApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4d2963)),
       ),
-      home: const HomeScreen(),
-      // By default, the app starts at the '/' route, which is the HomeScreen
+      home: HomeScreen(cartModel: cartModel),
       initialRoute: '/',
-      // When navigating to '/product', build and return the ProductPage
-      // In your browser, try this link: http://localhost:49856/#/product
       routes: {
-        '/product': (context) => const ProductPage(),
-        '/about': (context) => const AboutUsPage(),
-        '/cart': (context) => const CartPage(),
+        '/product': (context) => ProductPage(cartModel: cartModel),
+        '/about': (context) => AboutUsPage(cartModel: cartModel),
+        '/cart': (context) => CartPage(cartModel: cartModel),
       },
     );
   }
